c730e6988cf9f13cc7c73d2f27629b50d37e0699
d1n4ara <dandararamoscosta@gmail.com>
2025-06-05 23:14:39 -0300

    Deixando a dashboard dinâmica com dados provenientes do banco de dados

---
+++ src/database/config.js
@@ -0,0 +1,39 @@
+var mysql = require("mysql2");
+
+// Configurações do banco
+var mySqlConfig = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+};
+
+function executar(instrucao, params = []) {
+    if (process.env.AMBIENTE_PROCESSO !== "producao" && process.env.AMBIENTE_PROCESSO !== "desenvolvimento") {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM .env OU dev.env OU app.js\n");
+        return Promise.reject("AMBIENTE NÃO CONFIGURADO EM .env");
+    }
+
+    return new Promise(function (resolve, reject) {
+        var conexao = mysql.createConnection(mySqlConfig);
+        conexao.connect();
+
+        conexao.query(instrucao, params, function (erro, resultados) {
+            conexao.end();
+            if (erro) {
+                reject(erro);
+                return;
+            }
+            resolve(resultados);
+        });
+
+        conexao.on('error', function (erro) {
+            console.error("ERRO NO MySQL SERVER: ", erro.sqlMessage);
+        });
+    });
+}
+
+module.exports = {
+    executar
+};
